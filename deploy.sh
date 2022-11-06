c013769150af023a2f24b355a927e95f7b9ca780
Shahbanps <psshhaban@gmail.com>
2022-11-06 21:51:11 +0530

    added deploemet scripts attempt 3

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t shahban/multi-client:latest -t shahban/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t shahban/multi-server:latest -t shahban/multi-server:$SHA -f ./client/Dockerfile ./server
-docker build -t shahban/multi-worker:latest -t shahban/multi-worker:$SHA -f ./client/Dockerfile ./worker
+docker build -t shahban/multi-server:latest -t shahban/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t shahban/multi-worker:latest -t shahban/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push shahban/multi-client:latest
 docker push shahban/multi-server:latest
@@ -12,5 +12,5 @@ docker push shahban/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=shahban/multi-server:$SHA
-kubectl set image deployments/server-deployment client=shahban/multi-client:$SHA
-kubectl set image deployments/server-deployment worker=shahban/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=shahban/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=shahban/multi-worker:$SHA
