80bec02cafda208eb35a7415bf4b1b8f75f3d8dc
Shahbanps <psshhaban@gmail.com>
2022-11-19 12:36:11 +0530

    test1

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator Test 1
+            Fib Calculator Kubernetes
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
